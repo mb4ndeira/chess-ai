dfe9ced2ab36ef4914ac6280f309844f814864b0
Matheus Bandeira <mbandeira.cs@gmail.com>
2025-05-26 14:55:04 -0300

    fix: model training path

--- src/model/gaming_model.py
+++ src/model/gaming_model.py
@@ -8,24 +8,25 @@ from model.res_net import build_resnet
 class GamingRLModel():
     def __init__(self, model_path=None):
         default_model_path = "gaming_model.keras"
-
-        if model_path is not None:
-            model_path = os.path.join(os.getcwd(), model_path)  
-            if os.path.exists(model_path):
-                self._load_model(model_path)
-            else:
-                print("Model path not found. Defaulting to model_path:", default_model_path)
-                self._model_path = os.path.join(os.getcwd(), default_model_path)
-                self._model = self._build_model()
-
-        if model_path is None:
-            print("Model path not provided. Defaulting to model_path:", default_model_path)
+        def use_default_model(self, default_model_path):
             self._model_path = os.path.join(os.getcwd(), default_model_path)
             self._model = self._build_model()
 
+        if model_path is not None:
+            full_model_path = os.path.join(os.getcwd(), model_path)
+            if os.path.exists(full_model_path):
+                self._model_path = full_model_path
+                self._load_model(self._model_path)
+            else:
+                print(f"Model path '{full_model_path}' not found. Defaulting to '{default_model_path}'")
+                use_default_model(default_model_path)
+        else:
+            print(f"Model path not provided. Defaulting to '{default_model_path}'")
+            use_default_model(default_model_path)
+
         if list_physical_devices('GPU'):
             mixed_precision.set_global_policy('mixed_float16')
-
+        
     def _load_model(self, model_path):
         try:
             self._model = models.load_model(model_path)
@@ -52,16 +53,17 @@ class GamingRLModel():
         model = build_resnet(input_shape, action_size, num_resBlocks, num_hidden)
         return model
 
-    def train(self, train_data, epochs=10, batch_size=64, validation_data=None):
+    def train(self, train_data, epochs=100, batch_size=64, validation_data=None):
         """
         Train the model using the provided data.
 
-        train_data: Tuple containing the training data (inputs, (policy_target, value_target)).
+        train_data: Tuple containing the training data (inputs, policy_target, value_target).
         """
-        
+        print(self._model_path)
+
         X_train = np.array([step[0] for step in train_data], dtype=np.float32)
         policy_target = np.array([step[1] for step in train_data], dtype=np.float32)
-        value_target = np.array([step[2] for step in train_data], dtype=np.float32)
+        value_target = np.array([float(step[2]) if np.isscalar(step[2]) else float(step[2][0]) for step in train_data], dtype=np.float32)
         
         self._model.compile(optimizer='adam', loss=['categorical_crossentropy', 'mean_squared_error'])
 
@@ -71,6 +73,8 @@ class GamingRLModel():
             batch_size=batch_size,
             validation_data=validation_data
         )
+
+        self._save_model(self._model_path)
         
         return history
 
